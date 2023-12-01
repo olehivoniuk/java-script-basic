18404affb55867cc6c5025049c8b39fe29ccf914
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-01 15:57:54 +0200

    503- 1- task
    503- 1- task

---
+++ math.js
@@ -0,0 +1,30 @@
+;(function(){
+function avg1(arr) {
+	return sum(arr, 1) / arr.length;
+}
+
+function avg2(arr) {
+	return sum(arr, 2) / arr.length;
+}
+
+function avg3(arr) {
+	return sum(arr, 3) / arr.length;
+}
+
+// вспомогательная функция
+function sum(arr, pow) {
+	let res = 0;
+	
+	for (let elem of arr) {
+		res += elem ** pow;
+	}
+	
+	return res;
+}
+
+window.math = {avg1, avg2, avg3}
+
+
+})();
+
+console.log(window.math.avg1([1,2,3]));
