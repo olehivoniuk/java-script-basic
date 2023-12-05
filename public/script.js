985f50884c18f3ab46b0785f5784154bb128d641
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-05 15:29:55 +0200

    238-Submitting-form-via-JS

--- public/script.js
+++ public/script.js
@@ -1,1 +1,10 @@
-console.log("hey, welcome to the server ")
+
+
+let form   = document.querySelector('form');
+let link = document.querySelector('#submitButton');
+
+link.addEventListener('click', function(event) {
+    event.preventDefault(); 
+	let res = form.submit();
+    return res; 
+});
--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 export default {
-	'/handler/': function({get}) {
-		let arr = [
-			'user1', 'user2', 'user3'
-		];
-		if  (arr[Number(get.key )]){
-			return arr[Number(get.key )]
-		} else{
-			return 'check querry key'
-		}
+	'/handler/': function(data) {
+		console.log(data.get); 
+		return "Well"
 	}
 }
