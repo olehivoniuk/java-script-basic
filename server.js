12677e34ec1250af96cbf8bc17f04fac9fa306a3
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-05 14:05:31 +0200

    236-using-get-requests-2task

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 export default {
-	'/handler/': function(data) {
-		console.log(data.post);
-		let login = 'oleh91'; 
-		let password = 'oscar'
-
-		if(data.post.test1 == login && data.post.test2 == password){
-			return 'you have accsees to the site data'
-		}else{
-			return 'password is incorrect'
+	'/handler/': function({get}) {
+		let arr = [
+			'user1', 'user2', 'user3'
+		];
+		if  (arr[Number(get.key )]){
+			return arr[Number(get.key )]
+		} else{
+			return 'check querry key'
 		}
 	}
 }
