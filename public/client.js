2930193ceb9e17627ab1e8c18ad5bfc657e46053
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-05 18:17:07 +0200

    244-fetch-basics-with-AJAX-task2

--- public/client.js
+++ public/client.js
@@ -1,10 +1,9 @@
 
 let div    = document.querySelector('div');
-let button1 = document.querySelector('#button1');
-let button2 = document.querySelector('#button2');
-let button3 = document.querySelector('#button3');
+let button = document.querySelector('#button');
 
-button1.addEventListener('click', function() {
+
+button.addEventListener('click', function() {
 	fetch('./ajax1.html').then(
 		response => {
 			return response.text();
@@ -14,31 +13,35 @@ button1.addEventListener('click', function() {
 			div.innerHTML = text;
 		}
 	);
-});
 
-button2.addEventListener('click', function() {
-	fetch('./ajax2.html').then(
-		response => {
-			return response.text();
-		}
-	).then(
-		text => {
-			div.innerHTML = text;
-		}
-	);
-});
+    button.addEventListener('click', function() {
+        fetch('./ajax2.html').then(
+            response => {
+                return response.text();
+            }
+        ).then(
+            text => {
+                div.innerHTML = text;
+            }
+        );
+        button.addEventListener('click', function() {
+            fetch('./ajax3.html').then(
+                response => {
+                    return response.text();
+                }
+            ).then(
+                text => {
+                    div.innerHTML = text;
+                }
+            );
+        })
+    })})
 
-button3.addEventListener('click', function() {
-	fetch('./ajax3.html').then(
-		response => {
-			return response.text();
-		}
-	).then(
-		text => {
-			div.innerHTML = text;
-		}
-	);
-});
+
+
+
+
+
 
 
 
