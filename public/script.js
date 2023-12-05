3850c116f6a61a031e4eb101760c05cf9083e3fa
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-05 16:04:44 +0200

    239-preventing-form-submission-task1

--- public/script.js
+++ public/script.js
@@ -1,10 +1,23 @@
 
 
-let form   = document.querySelector('form');
-let link = document.querySelector('#submitButton');
+let form = document.querySelector('form');
+let input = document.getElementById('test')
+// // let link = document.querySelector('#submitButton');
 
-link.addEventListener('click', function(event) {
-    event.preventDefault(); 
-	let res = form.submit();
-    return res; 
-});
+// // link.addEventListener('click', function(event) {
+// //     event.preventDefault(); 
+// // 	let res = form.submit();
+// //     return res; 
+// // });
+
+form.addEventListener('submit', function(event) {
+	
+    let email = 'oscar@com'; 
+    if(email == input.value ){
+        return "Well"
+    }else{ 
+        event.preventDefault();
+        console.log("The email is incorrect, try again"); 
+    }
+    
+}); 
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 export default {
 	'/handler/': function(data) {
 		console.log(data.get); 
-		return "Well"
+
+		let email = 'oscar@com'; 
+		if(email == data.get.test1){
+			return "Well"
+		}else{ 
+			return "The email is incorrect, try again"
+		}
+		
 	}
 }
