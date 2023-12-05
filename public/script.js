67ee66c2a950f1ea2225e47b575a48869a02f3fc
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-05 16:38:47 +0200

    240-working-working-with-form-data

--- public/script.js
+++ public/script.js
@@ -1,23 +1,35 @@
-
-
+let button = document.querySelector('button'); 
 let form = document.querySelector('form');
-let input = document.getElementById('test')
-// // let link = document.querySelector('#submitButton');
 
-// // link.addEventListener('click', function(event) {
-// //     event.preventDefault(); 
-// // 	let res = form.submit();
-// //     return res; 
-// // });
 
-form.addEventListener('submit', function(event) {
+
+button.addEventListener('click', function(){
+    let formData = new FormData(form);
+console.log(formData);
+console.log(Array.from(formData));
+
+})
+
+
+
+// let form = document.querySelector('form');
+// let input = document.getElementById('test')
+// // // let link = document.querySelector('#submitButton');
+
+// // // link.addEventListener('click', function(event) {
+// // //     event.preventDefault(); 
+// // // 	let res = form.submit();
+// // //     return res; 
+// // // });
+
+// form.addEventListener('submit', function(event) {
 	
-    let email = 'oscar@com'; 
-    if(email == input.value ){
-        return "Well"
-    }else{ 
-        event.preventDefault();
-        console.log("The email is incorrect, try again"); 
-    }
+//     let email = 'oscar@com'; 
+//     if(email == input.value ){
+//         return "Well"
+//     }else{ 
+//         event.preventDefault();
+//         console.log("The email is incorrect, try again"); 
+//     }
     
-}); 
+// }); 
