662f7ba4584c8c091fc0d4ef259ed65e7351a4ef
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-06 10:32:05 +0200

    250-retreivingJSON-via-AJAX-inJS

--- public/client.js
+++ public/client.js
@@ -4,21 +4,21 @@ let button = document.querySelector('#button');
 
 
 button.addEventListener('click', function() {
-	fetch('./ajax1.html').then(
-		response => {  if (response.ok){
-            console.log(response.headers.get('Content-Length'))
-			return response.text();
-        } else {
-				throw new Error('bad response status');
-			}     
+	fetch('./data.json').then(response => {
+		return response.json();
+       
+	}).then(data => {
+		console.log(data);
+
+      
+        for (let dataCell of data){
+            const li = document.createElement('li'); 
+            li.textContent = dataCell; 
+            document.body.appendChild(li); 
+
         }
-	).then(
-		text => {
-			div.innerHTML = text;
-		}
-	).catch(
-       error  => {alert(error + ' ' + 'Network error')}
-    );
+        return data; 
+	});
 });
 
 
