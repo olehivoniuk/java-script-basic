d6274217ef3adc4ed6138244705aff7b88487353
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-08 17:20:12 +0200

    1.2-N2

--- public/client.js
+++ public/client.js
@@ -1,19 +1,14 @@
 
 function getFirstDigit(num){
 
-   let firstDigit =  num.toString().split('').slice(0,1)
+   let firstDigit =  num.toString().split('').slice(-1)
 
    console.log(Number(firstDigit))
    
-   
-   if (firstDigit != 'p' ){
-       console.log('the words ends with all letters but p')
-   }else{
-       console.log( str[str.length - 2])
-   }
+
 }
 
-getFirstDigit(444)
+getFirstDigit(443)
 
 
 
