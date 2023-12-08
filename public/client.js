b768dd27a5fa80adfa8efc47586f5915a69a81cd
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-08 20:34:25 +0200

    1.9-N2

--- public/client.js
+++ public/client.js
@@ -1,11 +1,11 @@
 
 function getArr(arr){
  
-    console.log(arr.filter( elem => elem.startsWith('http://')?elem : null))
+    console.log(arr.filter( elem => elem.endsWith('.html')?elem : null))
 
 }
 
-getArr(['1.456', 'http://2.125', '3.32', 'http://4.1', '5.34'])
+getArr(['1.456.html', 'http://2.125', '3.32.html', 'http://4.1', '5.34'])
 
 
 
