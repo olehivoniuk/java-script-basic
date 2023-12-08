452db7c5c5ba3fca851c1bad460c56de69e143a8
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-08 20:04:21 +0200

    1.6-N5

--- public/client.js
+++ public/client.js
@@ -1,18 +1,18 @@
 
-function gerReversed(str){
+function gerSum(arr){
 
-    let arr = str.split('')
-    console.log(arr)
-
-    for ( let i = arr.length - 1; i >= 0; i--){
-        console.log(arr[i])
+    let sum = 0; 
+    for ( let i = 0; i <= arr.length - 1; i++){
+        if (arr[i]>0 && arr[i]< 10){
+     
+        sum += arr[i] ; 
+        }
     }
-
-    
+    console.log(sum);
 
 }
 
-gerReversed("Hello")
+gerSum([2,4,5,6,-5,7,8])
 
 
 
