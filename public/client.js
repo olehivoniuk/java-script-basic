21c97773fe619daeeba081133cba31cc311efd34
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-08 16:55:05 +0200

    1.1-N1

--- public/client.js
+++ public/client.js
@@ -1,42 +1,15 @@
-let arr = [
-	{
-		id: 'GYi9GauC4gBF1e2SixDvu',
-		prop1: 'value11',
-		prop2: 'value12',
-		prop3: 'value13',
-	},
-	{
-		id: 'IWSpfBPSV3SXgRF87uO74',
-		prop1: 'value21',
-		prop2: 'value22',
-		prop3: 'value23',
-	},
-	{
-		id: 'JAmjRlfQT8rLTm5tG2m1L',
-		prop1: 'value31',
-		prop2: 'value32',
-		prop3: 'value33',
-	}
-];
 
-let id = 'IWSpfBPSV3SXgRF87uO74';
-let prop = 'prop1';
-let value = '!!!';
+function checkNumber(num){
+    if(num>0){
+        console.log('the number is positive')
+    }else{
+        console.log(
+            'the number is negatinve or 0'
+        )
+    }
+}
 
-let res = arr.map(elem => {
-	if (elem.id === id) {
-		return Object.assign({}, elem, { [prop]: value });
-	} else {
-		return elem;
-	}
-});
-
-console.log(res);
-
-
-
-
-
+checkNumber(-1)
 
 
 
