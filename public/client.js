195d25f55e6cb378e48c2f9bf62f83371bcf8bab
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-08 20:58:57 +0200

    1.10-N1

--- public/client.js
+++ public/client.js
@@ -1,24 +1,10 @@
+function getRandom(num){
 
-function getArr(arr){
+  let res =  Math.floor(Math.random() * num) + 1
 
-//   let arr1 = []
-
-//     for ( let i = 0; i < arr.length - 1; i++ ){
-//         arr1.push(arr[i] + arr[i]*0.1)
-//     }
-
-//     console.log(arr1);
-    
-    
-     let res = arr.map(elem =>  elem + elem * 0.1 )
-     console.log(res);
-
-
-   
+  console.log(res);
 }
-
-getArr([2,4,5,6])
-
+getRandom(100)
 
 
 
