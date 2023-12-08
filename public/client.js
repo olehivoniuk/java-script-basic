3d9bb65c6c2c242b5e3d0b9eef884ebe4dd91487
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-08 12:37:44 +0200

    280-Event-handlers-usage-optim-in-JS

--- public/client.js
+++ public/client.js
@@ -1,22 +1,26 @@
+let ul = document.querySelector('ul');
 
-let arr = fillArr('x', 5);
+let table = document.createElement('table'); 
+document.body.appendChild(table); 
 
-function fillArr(val, amount) {
-	let arr = [];
-	
-	for (let i = 1; i <= amount; i++) {
-		arr.push(val);
-	}
-	
-	return arr;
+
+for (let i = 1; i <= 100; i++) {
+	let tr = document.createElement('tr');
+	table.appendChild(tr);
+    for ( let k = 1; k <=100; k++){
+        let td = document.createElement('td')
+        td.textContent = k ; 
+        tr.appendChild(td)
+    }
 }
 
-const x = Array(5).fill("x", 0);
-console.log(x)
-
-
-
-
+table.addEventListener('click', function(event) {
+	let td = event.target.closest('td');
+	
+	if (td) {
+		td.style.color = 'red '
+	}
+});
 
 
 
