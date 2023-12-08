e0e48525a1357db8adb9126f27f4e8a30700c72d
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-08 21:17:34 +0200

    1.10-N4

--- public/client.js
+++ public/client.js
@@ -1,14 +1,10 @@
-function getSubarrays(arr){
+function getSubarrays(arr1, arr2){
 
-    let a = arr.slice(0,2)
+    let a = arr1.concat(arr2); 
     console.log(a);
-    let b = arr.slice(2,4)
-    console.log(b);
-    let c = arr.slice(4,6)
-    console.log(c);
  
 }
-getSubarrays([1, 2, 3, 4, 5, 6])
+getSubarrays([1, 2, 3], [4, 5, 6])
 
 
 
