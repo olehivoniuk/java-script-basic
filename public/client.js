7e2e40d5cd4dc0392a71985362c096306cf1fb82
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-08 20:12:39 +0200

    1.7-N2

--- public/client.js
+++ public/client.js
@@ -1,18 +1,14 @@
 
-function gerSum(arr){
+function gerSum(num){
 
-    let sum = 0; 
-    for ( let i = 0; i <= arr.length - 1; i++){
-        if (arr[i]>0 && arr[i]< 10){
-     
-        sum += arr[i] ; 
-        }
-    }
-    console.log(sum);
+      
+     let arr = num.toString().split(''); 
+     let sum = arr.reduce((sum, elem )=> sum + Number(elem),0)
+     console.log(sum);
 
 }
 
-gerSum([2,4,5,6,-5,7,8])
+gerSum(12345)
 
 
 
