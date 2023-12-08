0e96aa961d13bf7e0842977ee0cfca4b64e35321
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-08 13:52:34 +0200

    290-Adding-elem-with-ID-in-JS

--- public/client.js
+++ public/client.js
@@ -1,12 +1,62 @@
+
 let  button = document.querySelector('button'); 
 
-let arr = [1, 2, 3, 4, 5];
-let ind = 2;
+
+let arr = [
+	{
+		id: 'GYi9GauC4gBF1e2SixDvu',
+		prop1: 'value11',
+		prop2: 'value12',
+		prop3: 'value13',
+	},
+	{
+		id: 'IWSpfBPSV3SXgRF87uO74',
+		prop1: 'value21',
+		prop2: 'value22',
+		prop3: 'value23',
+	},
+	{
+		id: 'JAmjRlfQT8rLTm5tG2m1L',
+		prop1: 'value31',
+		prop2: 'value32',
+		prop3: 'value33',
+	},
+];
+
+let newElem = {
+	id: 'GMNCZnFT4rbBP6cirA0Ha',
+	prop1: 'value41',
+	prop2: 'value42',
+	prop3: 'value43',
+};
+
+function getId(length = 16) {
+	let chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	
+	let res = '';
+	
+	for (let i = 0; i < length; i++) {
+		res += chars[Math.floor(Math.random() * chars.length)];
+	}
+	
+	return res;
+}
+
 
 button.addEventListener('click', function(){
-    let res = [...arr.slice(0,ind), "!", ...arr.slice(ind)]
-    console.log(res);
+    let copy = Object.assign([], arr);
+copy.push(newElem);
+
+for ( let i = 0 ; i < copy.length; i++){
+    copy[i].id = getId()
+
+}
+let res = copy;
+console.log(res);
 })
+
+
+
 
 
 
