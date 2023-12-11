2b4ddcbe5a46469c4e208024df58482997ccd351
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-11 19:44:54 +0200

    2.2-N2

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 
 let numbers = [1, -2, 3, -4, 5, -6];
 
-let belowZero = numbers.filter(num => num < 0);
+let belowZero = numbers.filter(num => num > 0);
 
-console.log(belowZero.length);
+console.log(belowZero);
