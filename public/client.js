8473bd5c1a79db58bedd0b360be6f3999fb9a5ea
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-11 19:43:47 +0200

    2.2-N1

--- public/client.js
+++ public/client.js
@@ -1,13 +1,6 @@
-function getSumOfTheHalf(arr){
 
-    let half = arr.slice(0, arr.length / 2) 
+let numbers = [1, -2, 3, -4, 5, -6];
 
-   let res = half.reduce((sum, elem) => sum + elem, 0) 
-    
-    console.log(res);
+let belowZero = numbers.filter(num => num < 0);
 
-}
-getSumOfTheHalf([1, 2, 3, 4, 5, 6])
-
-
-
+console.log(belowZero.length);
