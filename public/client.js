f0c6a6cde7e446f7d9cf49ea6773cbb43bdb7dbf
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-12 17:32:33 +0200

    2.6-N5

--- public/client.js
+++ public/client.js
@@ -1,14 +1,14 @@
-let str = 'AbCdE';
- 
-let newStr = "";
-for (let char of str) {
-    let upper = char.toUpperCase();
- 
-    if (char === upper) {
-        newStr += char.toLowerCase();
-    } else {
-        newStr += upper;
+function mergePairs(arr) {
+    let result = [];
+
+    for (let i = 0; i < arr.length; i += 2) {
+        let pair = arr.slice(i, i + 2);
+        result.push(parseInt(pair.join('')));
     }
+
+    return result;
 }
-// "hELLO, WORLD!"
-console.log(newStr);
+
+let inputArray = [1, 2, 3, 4, 5, 6];
+let mergedPairs = mergePairs(inputArray);
+console.log(mergedPairs);
