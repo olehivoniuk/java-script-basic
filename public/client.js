19cc4df6d5edc4e837205a7bd09e9b3f59d576b2
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-12 15:39:46 +0200

    2.5-N3

--- public/client.js
+++ public/client.js
@@ -1,16 +1,23 @@
 
-function convertToUpperCase(str) {
+function getDifference(arr) {
 
-    let arr = str.split('')
+    let sum1 = 0
 
-    for ( let i = 3; i < arr.length; i+=2){
-          console.log(arr[i]);
+    for ( let i = 0; i <= arr.length - 1; i+=2){
+            sum1 += arr[i]
         
     }
-    
 
+    let sum2 = 0
+    for ( let i = 1; i <= arr.length - 1; i+=2){
+       
+        sum2 += arr[i]
+    
+}
+console.log(sum1/sum2)
+    
 }
 
-convertToUpperCase('abcdefg')
+getDifference([1, 2, 3, 4, 5, 6]);
 
 
