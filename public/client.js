046b3258478bca0f8c2a57550ebe0a1cc10a5255
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-14 14:30:31 +0200

    2.9-N2

--- public/client.js
+++ public/client.js
@@ -1,16 +1,9 @@
-function getTotalOfPairs(num) {
+function getTotalOfPairs(arr) {
 
-  let arr = num.toString().split('');
+  let res = arr.reverse()
 
-   let result = [];
-
-    for (let i = 0; i < arr.length; i += 2) {
-        let pair = arr.slice(i, i + 2);
-        result.push(parseInt(pair.join('')));
-    }
-
-  console.log(result.reduce((sum,elem) => sum + elem,0))
+  console.log(res);
  
 }
 
-getTotalOfPairs(123456)
+getTotalOfPairs([1, 2, 3, 4, 5])
