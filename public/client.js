6dd2c6ab289bfcfbdc9631d0082670607ed33fb5
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-14 13:15:18 +0200

    2.7-N8

--- public/client.js
+++ public/client.js
@@ -1,14 +1,12 @@
-function moveToUpperCase(str) {
-  let words = str.split(' ');
-  
+function moveToUpperCase(symb) {
+ 
+  if ( symb == symb.toUpperCase()){
 
-  for (let i = 0; i < words.length; i += 1) {
-      if (words[i].length <= 3) {
-          words[i] = words[i].toUpperCase()
-      }
+
+    console.log("the symbol is in upper case ");
+  } else {
+    console.log("The sypmbol is in lower case ")
   }
-
-    console.log (words.join(' '))
 }
 
-moveToUpperCase('a bc def ghij');
+moveToUpperCase('O');
