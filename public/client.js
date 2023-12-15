afbda8b10edd7b734e96d0893668b17fe6f95a46
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-15 13:20:04 +0200

    3.1-N2

--- public/client.js
+++ public/client.js
@@ -1,15 +1,10 @@
-function checkProgression(num){
+function deleteSpaces(arr){
 
-   let arr = num.toString().split('')
+   let arr1 = arr.join('').split('')
 
-   for ( let i = 0; i< arr.length; i++){
-     if ( arr[i] > arr [i + 1]){
-       return false
-     } else{
-       return true
-     }
-   }
+   console.log(arr1)
+
+   
 }
 
-let res = checkProgression(1123451); 
-console.log(res)
+ deleteSpaces([1, '', 2, 3, '', 5]); 
