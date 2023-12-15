2b7a11419d8279b6116a59096e2909d8d7ffcbb4
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-15 19:55:39 +0200

    3.4-N2

--- public/client.js
+++ public/client.js
@@ -1,20 +1,41 @@
 
 
-function changeArraysOrder(arr){
+function findTheSumOfElements(obj){
 
- let arr1 = []; 
+  let sum = 0; 
 
-  for ( let i = 0 ; i < arr.length; i+=2){
-      arr1.push( arr.slice(i, i+2).reverse());
+  for ( let key in obj){
+    let subObj = obj[key]; 
+
+    for ( let key in  subObj){
+      let value = subObj[key]
+
+      sum += value
+
+    }
   }
 
-   console.log (arr1.join(',').split(','))
- 
-  return arr1
-  
+  return sum
 
-  
+ 
 }
 
- changeArraysOrder([1, 2, 3, 4, 5, 6])
+   let res  = findTheSumOfElements( {
+	1: {
+		1: 11,
+		2: 12,
+		3: 13,
+	},
+	2: {
+		1: 21,
+		2: 22,
+		3: 23,
+	},
+	3: {
+		1: 24,
+		2: 25,
+		3: 26,
+	},
+})
  
+console.log(res);
