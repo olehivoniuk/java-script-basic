4587443e44fdb2188c070c3107e81c3f0dcd9231
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-15 21:01:28 +0200

    3.5-N7

--- public/client.js
+++ public/client.js
@@ -1,21 +1,122 @@
+// let obj = {
+// 	1: {
+// 		1: {
+// 			1: 111,
+// 			2: 112,
+// 			3: 113,
+// 		},
+// 		2: {
+// 			1: 121,
+// 			2: 122,
+// 			3: 123,
+// 		},
+// 	},
+// 	2: {
+// 		1: {
+// 			1: 211,
+// 			2: 212,
+// 			3: 213,
+// 		},
+// 		2: {
+// 			1: 221,
+// 			2: 222,
+// 			3: 223,
+// 		},
+// 	},
+// 	3: {
+// 		1: {
+// 			1: 311,
+// 			2: 312,
+// 			3: 313,
+// 		},
+// 		2: {
+// 			1: 321,
+// 			2: 322,
+// 			3: 323,
+// 		},
+// 	},
+// }
 
 
-function createHyphenatedSting(num){
+// function findObjSum(obj){
 
-  let arr = []
+//   let sum = 0; 
 
-  for ( let i = 1; i <= num; i++ ){
-    arr.push(`-${i}`)
-    
-  }
+//   for ( let key in obj){
+//     let subObj = obj[key]
+//       for ( let key1 in subObj){
+//         let subSubObj = obj[key][key1]
+//           for ( let key2 in subSubObj){
+//             let value = subSubObj[key2]
 
-  return arr.join('')
+//             sum += value
+//           }
+
+//     }
+//   }
+
+//   return sum
+
+
+
+// }
+
+//  let res = findObjSum(obj); 
+
+//  console.log(res);
+
+let obj = {
+	1: {
+		1: {
+			1: 111,
+			2: 112,
+			3: 113,
+		},
+		2: {
+			1: 121,
+			2: 122,
+			3: 123,
+		},
+	},
+	2: {
+		1: {
+			1: 211,
+			2: 212,
+			3: 213,
+		},
+		2: {
+			1: 221,
+			2: 222,
+			3: 223,
+		},
+	},
+	3: {
+		1: {
+			1: 311,
+			2: 312,
+			3: 313,
+		},
+		2: {
+			1: 321,
+			2: 322,
+			3: 323,
+		},
+	},
+};
+
+function sumObject(obj) {
+	let sum = 0;
+
+	for (let key in obj) {
+		if (typeof obj[key] === 'object') {
+			sum += sumObject(obj[key]);
+		} else {
+			sum += obj[key];
+		}
+	}
+
+	return sum;
 }
 
- let res = createHyphenatedSting(5)
-
- console.log(`${res}-`);
-
-// let num = 5;
-// let zerosString = '-0'.repeat(num);
-// console.log(`${zerosString}-`);
+let totalSum = sumObject(obj);
+console.log(totalSum);
