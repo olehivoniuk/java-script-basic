67adff20ec24ed2b6313b4195780376f99f4af17
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-15 20:35:46 +0200

    3.5-N4

--- public/client.js
+++ public/client.js
@@ -2,10 +2,10 @@
 
 function filterArray(arr){
 
-   let res  = arr.filter(elem =>  (elem.toString().includes('0'))? elem : null)
+   let res  = arr.some(elem =>  (elem.toString().includes('3'))? true : false)
 
    console.log(res);
   
 }
 
-filterArray([1,5,25,20,50,41,40,35])
+filterArray([1,5,25,20,50,41,40,5])
