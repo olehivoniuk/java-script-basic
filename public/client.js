5a23cdd760d9babb11ce22c8dbad38148a0b6a58
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-15 15:57:09 +0200

    3.3-N4

--- public/client.js
+++ public/client.js
@@ -1,23 +1,38 @@
-function checkIfPalindrome(str){
+function findArraysTotal(arr){
 
-  let arr1 = str.split('').reverse().join(''); 
 
-  let arr = str.split('').join('')
+    let sum = 0; 
 
-     
+    for ( let i = 0; i < arr.length; i++){
+      for( let j = 0; j < arr[i].length; j++){
+        for ( let k = 0; k < arr[i][j].length; k++){
+           sum += arr[i][j][k]
+        }
+      }
+    }
 
-   
-    console.log(arr)
+    return sum; 
 
-    console.log(arr1)
-
-    console.log(arr == arr1)
-  
 
 }
-  checkIfPalindrome('abcba')
+  let res = findArraysTotal([
+    [
+      [11, 12, 13],
+      [14, 15, 16],
+      [17, 17, 19],
+    ],
+    [
+      [21, 22, 23],
+      [24, 25, 26],
+      [27, 27, 29],
+    ],
+    [
+      [31, 32, 33],
+      [34, 35, 36],
+      [37, 37, 39],
+    ],
+  ])
 
 
-
-
+console.log(res)
 
