5186240927501007fe784d33eea82aea682c6c99
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-15 19:16:16 +0200

    3.4-N1

--- public/client.js
+++ public/client.js
@@ -1,38 +1,15 @@
-function findArraysTotal(arr){
 
 
-    let sum = 0; 
+function getNumbers(num){
 
-    for ( let i = 0; i < arr.length; i++){
-      for( let j = 0; j < arr[i].length; j++){
-        for ( let k = 0; k < arr[i][j].length; k++){
-           sum += arr[i][j][k]
-        }
-      }
-    }
+  
 
-    return sum; 
+  for (let i = 10; i < num; i++ ){
+  if (i.toString().split('')[0] %2 == 0){
+    console.log(i);
+  }
 
-
+  }
 }
-  let res = findArraysTotal([
-    [
-      [11, 12, 13],
-      [14, 15, 16],
-      [17, 17, 19],
-    ],
-    [
-      [21, 22, 23],
-      [24, 25, 26],
-      [27, 27, 29],
-    ],
-    [
-      [31, 32, 33],
-      [34, 35, 36],
-      [37, 37, 39],
-    ],
-  ])
 
-
-console.log(res)
-
+getNumbers(1000)
