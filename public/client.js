ba3f1d29cf5045ca78454257de9b8674f9b796b5
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-17 15:49:04 +0200

    3.7-N4

--- public/client.js
+++ public/client.js
@@ -1,25 +1,8 @@
 
-function getDivisors(num){
 
-  let arr = [];
-
-  for ( let i = 0; i<= num; i++){
-    if (  num % i == 0 ){
-      arr.push(i)
-    }
-  }
-
- if ( arr.length == 3 ){
-   return true
- } else{
-   return false
- }
  
  
- }
- 
- 
-  let res = getDivisors(4); 
+  let res = Math.max(12,4,6,7,8,9); 
 
   console.log(res);
 
