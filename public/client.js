bc7ae5053f069d34b7929e7b097b42e3e31f902f
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-17 20:20:19 +0200

    3.10-N2

--- public/client.js
+++ public/client.js
@@ -1,13 +1,21 @@
 
 
-function getRepeated(arr){
+function getRepeated(arr, num){
+
+  let arr1 = []; 
 
   for (let i = 0; i < arr.length; i++){
-    arr[i] = `${arr[i]},${arr[i]}`
+
+    if ( num % arr[i] == 0){
+      arr1.push(arr[i])
+    }
+   
   }
-  console.log(arr);
+
+  console.log(arr1)
+  
 
 }
  
 
-getRepeated(['2', '4', '5'])
+getRepeated(['2', '4', '5', '123'], 10)
