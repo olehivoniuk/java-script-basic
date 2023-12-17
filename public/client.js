f6229df760447d3cbc00d8aabe56602165bde0dd
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-17 16:12:04 +0200

    3.7-N6

--- public/client.js
+++ public/client.js
@@ -2,7 +2,23 @@
 
  
  
-  let res = Math.max(12,4,6,7,8,9); 
+ function getUpdatedArray(arr){
 
-  console.log(res);
+   let arr1 = arr.toString().split(',')
 
+
+   for ( let i = 0; i < arr1.length; i++){
+    if (arr1[i].toString().length == 1 ){
+        arr1[i] = `${arr1[i]}, ${arr1[i]}`
+    }
+   }
+
+   return arr1
+
+
+ }
+
+
+  let res = getUpdatedArray([1,23,5,52,65,77])
+
+  console.log(res)
