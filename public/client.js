bc67afeb6f97dfaaeafae1e333f6266efaf7b50b
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-17 19:54:09 +0200

    3.9-N3

--- public/client.js
+++ public/client.js
@@ -1,10 +1,20 @@
-function checkNumbers(str){
+function deleteElements(num){
 
-  let arr = str.split('')
-  let res = arr.every(elem => elem %2 ==  0)
-  console.log(res);
+  let arr = num.toString().split(',')
 
+ 
+  
+  for ( let i = 0; i< arr.length; i++){
+     let arr1 = arr[i].split(''); 
+     if ( !arr[i].includes('00')){
+       console.log(arr1)
+     }
+   
+  }
+
+  
 }
 
- checkNumbers('2422'); 
 
+deleteElements([32, 24000, 420, 410000]); 
+ 
