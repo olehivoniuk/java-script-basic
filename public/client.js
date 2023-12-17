2d0b3d31f3a6ec1d0e900ace3b1dda461e3b257e
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-17 15:45:25 +0200

    3.7-N3

--- public/client.js
+++ public/client.js
@@ -4,18 +4,22 @@ function getDivisors(num){
   let arr = [];
 
   for ( let i = 0; i<= num; i++){
-    if ( num % i == 0){
+    if (  num % i == 0 ){
       arr.push(i)
     }
   }
 
-  return arr;
+ if ( arr.length == 3 ){
+   return true
+ } else{
+   return false
+ }
  
  
  }
  
  
-  let res = getDivisors(15); 
+  let res = getDivisors(4); 
 
   console.log(res);
 
