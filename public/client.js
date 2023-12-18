fc55692c44c1f67e7f4e3d4dd8866de546680b00
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-18 13:32:16 +0200

    3.10-N6

--- public/client.js
+++ public/client.js
@@ -1,31 +1,23 @@
+arr = [
+	[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9],
+]
 
+function getConcatenated(arr){
 
-function getArray(arr){
-
-  let arr1 = arr.toString().split(',')
-
-  let arr2 = [];
-  let arrs2;
-
-
-  for ( let i = 0; i< arr1.length; i++){
-    arrs2 = arr1[i].split('').sort(); 
-   let arr3 = []
-   for ( let j = 0; j < arrs2.length;  j++){
-
-    if (arrs2[j] !=  arrs2[j + 1]){
-     arr3.push(arrs2[j]) 
-    }
-   }
-   arr2.push(arr3.join(''))
-  }
-
-  return arr2
- 
-}
- 
-  let res = getArray([234235,234,454,6767,345,12345,5656]); 
-  console.log(res)
-
-
+  let arr1 = []
   
+
+  for ( let i = 0; i < arr.length; i++){
+    let str =  arr[i].join('')
+
+    arr1.push(str)
+  }
+  return arr1.join('').split('')
+
+}
+
+ let res = getConcatenated(arr)
+
+  console.log(res)
