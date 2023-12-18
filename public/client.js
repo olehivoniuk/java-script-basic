9f0bab9a6217903d87faf57627eaee49e3deb971
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-18 13:05:22 +0200

    3.10-N5

--- public/client.js
+++ public/client.js
@@ -1,23 +1,31 @@
 
 
-function getArray(num1){
+function getArray(arr){
 
-  let arr1 = num1.toString().split('')
-  console.log(arr1);
-  let arr = []; 
+  let arr1 = arr.toString().split(',')
+
+  let arr2 = [];
+  let arrs2;
+
 
   for ( let i = 0; i< arr1.length; i++){
-    if ( arr1[i] == 3 ){
-      arr.push(i)
+    arrs2 = arr1[i].split('').sort(); 
+   let arr3 = []
+   for ( let j = 0; j < arrs2.length;  j++){
+
+    if (arrs2[j] !=  arrs2[j + 1]){
+     arr3.push(arrs2[j]) 
     }
+   }
+   arr2.push(arr3.join(''))
   }
 
-  arr.splice(0,1); 
-  arr.splice(arr.length - 1); 
-  return arr;
-  
-
+  return arr2
+ 
 }
  
-  let res =  getArray(2312352923224232); 
+  let res = getArray([234235,234,454,6767,345,12345,5656]); 
   console.log(res)
+
+
+  
