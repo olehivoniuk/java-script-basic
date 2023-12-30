1836d67a81739379444eeb6171d56f151fd6df24
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2023-12-30 19:32:31 +0200

    node.js-3-7

--- index.js
+++ index.js
@@ -1,110 +1,31 @@
 import fs, { fdatasync } from 'fs';
-// import { square, cube } from './math.js';
 
-// let res  = square(2) + cube(3);
-// console.log(res);
+fs.promises.readFile('file1.txt', 'utf8')
+  .then(data => {
+    let res = data.trim().split(',').map(Number); // Convert each character to a number
+    console.log(res);
 
-// let text1 = fs.readFileSync('file1.txt','utf8'); 
-// let text2 = fs.readFileSync('file2.txt','utf8'); 
+    function createFiles() {
+      let fileNames = [];
+      for (let i = 0; i < res.length; i++) {
+        fileNames.push(`text${i}.txt`);
+      }
+      return fileNames;
+    }
 
-// console.log(+text1 + +text2)
+    let fileNames = createFiles();
 
-// let obj = {
-// 	'file1.txt': 'text1',
-// 	'file2.txt': 'text2',
-// 	'file3.txt': 'text3',
-// }
+    function getContent(index) {
+      return res[index].toString(); // Convert the number to a string
+    }
 
-// for ( let [key, value] of Object.entries(obj)){
-// 	fs.writeFileSync(key,value)
-// }
-
-// setInterval(function(){
-// 	const currentContent = fs.readFileSync('file1.txt','utf-8'); 
-// 	const updatedContent = currentContent + "!"; 
-// 	fs.writeFileSync('file1.txt', updatedContent)
-// },1000)
-
-// try {
-// 	let text = fs.readFileSync('file.txt', 'utf8');
-// 	console.log(text);
-// } catch (err) {
-// 	console.log('при чтении файла возникла ошибка', err);
-// } 
-
-// fs.readFile('file.txt', 'utf-8', function(err, data){
-
-// 	if(!err){
-// 		console.log(data**2)
-// 	}else{
-// 		console.log("File does not exist", err)
-// 	}
-// })
-
-// console.log("Hello world")
-
-// function createFiles() {
-// 	let fileNames = [];
-// 	for (let i = 0; i < 10; i++) {
-// 	  fileNames.push('text' + i + '.txt');
-// 	}
-// 	return fileNames;
-//   }
-  
-//   function getContent() {
-// 	let fileContents = [];
-// 	for (let i = 0; i < 10; i++) {
-// 	  fileContents.push(i);
-// 	}
-// 	return fileContents.join('\n');
-//   }
-  
-//   let fileNames = createFiles();
-  
-//   fileNames.forEach((fileName) => {
-// 	fs.writeFile(fileName, getContent(), function (err) {
-// 	  if (err) {
-// 		console.log('Error creating file ' + fileName + ': ' + err.message);
-// 	  } else {
-// 		console.log('File ' + fileName + ' created successfully.');
-// 	  }
-// 	});
-//   });
-
-// fs.readFile('file1.txt', 'utf8', function(err, data1) {
-//     if (!err) {
-//         fs.readFile('file2.txt', 'utf8', function(err, data2) {
-//             if (!err) {
-//                 fs.readFile('file3.txt', 'utf8', function(err, data3) {
-//                     if (!err) {
-//                         fs.readFile('file4.txt', 'utf8', function(err, data4) {
-//                             if (!err) {
-//                                 fs.readFile('file5.txt', 'utf8', function(err, data5) {
-//                                     if (!err) {
-//                                         console.log(data1 * data2 * data3 * data4 * data5);
-//                                     } else {
-//                                         console.log('ошибка чтения файла file5');
-//                                     }
-//                                 });
-//                             } else {
-//                                 console.log('ошибка чтения файла file4');
-//                             }
-//                         });
-//                     } else {
-//                         console.log('ошибка чтения файла file3');
-//                     }
-//                 });
-//             } else {
-//                 console.log('ошибка чтения файла file2');
-//             }
-//         });
-//     } else {
-//         console.log('ошибка чтения файла file1');
-//     }
-// });
-
-fs.promises.readFile('file1.txt', 'utf8').then(data => {
-	 let res = data.toString().split(''); 
-	 let total = res.reduce((sum,elem)=> sum + Number(elem),0)
-	 console.log(total);
-});
+    return Promise.all(fileNames.map((fileName, index) => {
+      return fs.promises.writeFile(fileName, getContent(index));
+    }));
+  })
+  .then(() => {
+    console.log('Files created successfully.');
+  })
+  .catch(err => {
+    console.error('Error:', err);
+  });
