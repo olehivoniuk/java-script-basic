cdca6c08b4ef142b57cad10dbec95fcd2da167f4
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2024-01-01 18:23:49 +0200

    Node.js-server-Understanding-17-1

--- index.js
+++ index.js
@@ -1,54 +1,21 @@
-import fs from 'fs/promises';
-import { access, constants } from 'fs';
 
-// async function func() {
-//     try {
-//         let names = ['Hello1.txt', 'Hello2.txt','Hello3.txt','Hello4.txt', ];
-//        names.forEach((name)=>{
-// 		   let rand = Math.floor(Math.random()* names.length); 
-// 		    fs.promises.writeFile(name, rand.toString())
-// 	   })
-// 	   let data  = [];
-// 			for ( let name of names){
-//                data.push(await fs.promises.readFile(name, "utf-8"))
-// 			}
-// 			let updatedData = data.map(Number); 
-// 			let total = updatedData.reduce((sum, elem) => elem + sum, 0); 
-// 			console.log(total);
-
-// 			await fs.promises.writeFile('result.txt', total.toString())
-			
-//     } catch (err) {
-//         console.log('что-то пошло не так');
-//     }
-// }
-
-// func();
-
-// console.log(__dirname);
+import http from 'http';
 
 
+let i = 10;
 
- async function  checkExistence() {
-    // fs.access('result.txt', constants.F_OK)
-    //     .then(() => {
-    //         return fs.readFile('result.txt', 'utf-8');
-    //     })
-    //     .then((data) => {
-    //         console.log('File exists. Content:', data);
-    //     })
-    //     .catch(() => {
-    //         console.log('File does not exist');
-    //     });
-	try{
-		await fs.access("result.txt", constants.F_OK)
-        console.log("file exists")
-		let data = await fs.readFile ( "result.txt", 'utf-8')
-		console.log(data)
-	} catch(error){
-		console.log("the file does not exist", error)
+http.createServer((request, response) => {
+	response.setHeader('Content-Type', 'text/html');
+	response.statusCode = 200;
+	
+	if(i<0 ){
+		response.write("Amount of requests is exceeded")
+	} else{
+		response.write(String(i--));
 	}
-}
+	response.end();
+}).listen(3000);
 
-checkExistence();
+
+
 
--- server.js
+++
@@ -1,9 +0,0 @@
-export default {
-	'/handler/': function({post}) {
-	let num = Number(post.num1)+ Number(post.num2) + Number(post.num3) + Number(post.num4) + Number(post.num5)
-	 let avr = num / 5;
-	 console.log(avr);
-
-		return avr; 
-	}
-}
