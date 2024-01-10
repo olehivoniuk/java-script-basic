57fe27e5fb07ad6090533a980e2353c8060c0cdb
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2024-01-10 19:30:10 +0200

    checklist-project

--- workingApps/React/my-app/src/App.js
+++ workingApps/React/my-app/src/App.js
@@ -1,27 +1,61 @@
-
-import React from 'react';
-import './styles.css';
-import {useState} from 'react'
-import { useCallback } from 'react';
-import Products from './Products';
-
+import React, { useState } from 'react';
+import './styles.css'
 
 function App() {
-	const[text, setText] = useState('text'); 
-	const[products, setProducts ] = useState([])
-
-	const addProduct = useCallback(() =>{
-       setProducts(()=> [...products, "orange"])
-	}, [products] ) 
+  const [deed, setDeed] = useState('');
+  const [value, setValue] = useState([]);
+  const [ checked, setChecked] = useState(false)
 
 
-	return <>
+  function addDeed(event) {
+    setDeed(event.target.value);
+  }
 
-	<p onClick={()=> setText(text + "!")}>{text}</p>
-	<Products  products={products} addProduct={addProduct}/>
-	
-	</>
+  function addToDo() {
+    setValue([...value, deed]);
+    setDeed(''); // Clear the input after adding a deed
+  }
 
+  function deleteData(index) {
+	const updatedList = [...value];
+	updatedList.splice(index, 1);
+	setValue(updatedList);
+  }
+
+  function checkboxStatus(){
+
+	if(checked)
+	setChecked(!checked)
+  }
+
+
+  const result = value.map((item, index) => (<div>
+	   
+    	<li key={index} >
+		<input type='checkbox' checked={checked[index]} onChange={checkboxStatus} 
+		style={{ textDecoration: checked ? 'line-through' : 'none' }}
+		/>
+        {item}
+		
+		<button onClick={() => deleteData(index)}>Delete</button>
+		</li>
+		
+  </div>
+
+  ))
+
+  return (
+    <>
+      <p>Checklist project</p>
+      <input value={deed} onChange={addDeed} />
+      <button onClick={addToDo}>add to list</button>
+      <ul >
+        {result}
+      </ul>
+    </>
+  );
 }
 
 export default App;
+
+// style={{ textDecoration: checked[index] ? 'line-through' : 'none' }}
